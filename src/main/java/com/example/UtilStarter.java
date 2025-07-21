ea137279bc43db0d34f726d0918f01823d229535
infernaaa <Dotaah@mail.ru>
2025-07-22 01:51:51 +0700

    поправил mvn

--- src/main/java/com/example/UtilStarter.java
+++ src/main/java/com/example/UtilStarter.java
@@ -8,8 +8,6 @@ import picocli.CommandLine.Command;
 public class UtilStarter {
 
     public static void main(String[] args) {
-        String commandIn = "-sfa -p test_ -o G:\\testFor in3.txt in2.txt in1.txt";
-        args = commandIn.split(" ");
         int exitCode = new CommandLine(new DataCollectorCommand()).execute(args);
         System.exit(exitCode);
     }
